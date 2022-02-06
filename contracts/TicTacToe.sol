0e564400026b1f9765ae3786ce7105a82628de13
DONGCHI LI <dl1321@ic.ac.uk>
2022-02-06 18:59:16 +0000

    first finish

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.4.24;
  * @title TicTacToe contract
  **/
 contract TicTacToe {
-    address[2] public players;
+    address[2] public players;//two players
 
     /**
      turn
@@ -49,6 +49,11 @@ contract TicTacToe {
       **/    
     function _threeInALine(uint a, uint b, uint c) private view returns (bool){
         /*Please complete the code here.*/
+        if(board[a]==board[b] && board[a]==board[c]){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     /**
@@ -58,6 +63,45 @@ contract TicTacToe {
      */
     function _getStatus(uint pos) private view returns (uint) {
         /*Please complete the code here.*/
+        uint result;
+        bool full =true;
+        uint win = 0;
+
+        //check is the board full
+        for(uint i=0;i<9;i++){
+            if(board[i]==0){
+                full = false;
+                break;
+            }
+        }
+
+        if (((pos == 0) && (_threeInALine(0,1,2) || _threeInALine(0,3,6) || _threeInALine(0,4,8)))
+        || ((pos == 1) && (_threeInALine(0,1,2) || _threeInALine(1,4,7)))
+        || ((pos == 2) && (_threeInALine(0,1,2) || _threeInALine(2,5,8) || _threeInALine(2,4,6)))
+        || ((pos == 3) && (_threeInALine(3,4,5) || _threeInALine(0,3,6)))
+        || ((pos == 4) && (_threeInALine(1,4,7) || _threeInALine(3,4,5) || _threeInALine(0,4,8) || _threeInALine(2,4,6)))
+        || ((pos == 5) && (_threeInALine(2,5,8) || _threeInALine(3,4,5)))
+        || ((pos == 6) && (_threeInALine(6,7,8) || _threeInALine(0,3,6) || _threeInALine(2,4,6)))
+        || ((pos == 7) && (_threeInALine(1,4,7) || _threeInALine(6,7,8)))
+            || ((pos == 8) && (_threeInALine(6,7,8) || _threeInALine(2,5,8) || _threeInALine(0,4,8)))){
+            win = board[pos];
+        }
+
+        if(full){
+            if(win!=0){
+                result = win ;
+            }else{
+                result = 3;
+            }
+        }else{
+            if(win!=0){
+                result = win ;
+            }else{
+                result = 0;
+            }
+        }
+
+        return result;
     }
 
     /**
@@ -67,6 +111,9 @@ contract TicTacToe {
      */
     modifier _checkStatus(uint pos) {
         /*Please complete the code here.*/
+        require(status==0);
+        _;
+        status = _getStatus(pos);
     }
 
     /**
@@ -75,6 +122,8 @@ contract TicTacToe {
      */
     function myTurn() public view returns (bool) {
        /*Please complete the code here.*/
+    //return turn==1;
+    return players[turn - 1] == msg.sender;
     }
 
     /**
@@ -83,6 +132,13 @@ contract TicTacToe {
      */
     modifier _myTurn() {
       /*Please complete the code here.*/
+        require(myTurn()==true);
+        _;
+        if(turn == 1){
+            turn = 2;
+        }else{
+            turn = 1;
+        }
     }
 
     /**
@@ -92,6 +148,12 @@ contract TicTacToe {
      */
     function validMove(uint pos) public view returns (bool) {
       /*Please complete the code here.*/
+        //if board[pos] != null return false; else return true;
+        if(pos < board.length && pos >= 0 && board[pos] == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     /**
@@ -100,6 +162,8 @@ contract TicTacToe {
      */
     modifier _validMove(uint pos) {
       /*Please complete the code here.*/
+        require(validMove(pos)==true);
+        _;
     }
 
     /**
